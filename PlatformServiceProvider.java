8e6f7d960bcd79bdb2d731fc0ee0bf8dc1389178
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-21 19:16:05 +0900

    feat: 사용자 계정 등록 구현

--- PlatformServiceProvider.java
+++ PlatformServiceProvider.java
@@ -1,2 +1,24 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PlatformServiceProvider {
+    private Database db;
+
+    public PlatformServiceProvider(Database db) {
+        this.db = db;
+    }
+
+    public void registerUser(String username, String password, String role) {
+        String sql = "INSERT INTO User (username, password, role) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = this.db.connection.prepareStatement(sql);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, role);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error executing SQL query.");
+            e.printStackTrace();
+        }
+    }
 }
