d7cb3a9735d88d838ab95be0c532e800da4b74db
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-21 19:12:20 +0900

    feat: 배달 상태 업테이트 구현

--- DeliveryPerson.java
+++ DeliveryPerson.java
@@ -1,2 +1,23 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DeliveryPerson {
+    private Database db;
+
+    public DeliveryPerson(Database db) {
+        this.db = db;
+    }
+
+    public void updateDeliveryStatus(int orderId, String status) {
+        String sql = "UPDATE `Order` SET order_status = ? WHERE order_id = ?";
+        try {
+            PreparedStatement preparedStatement = this.db.connection.prepareStatement(sql);
+            preparedStatement.setString(1, status);
+            preparedStatement.setInt(2, orderId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error executing SQL query.");
+            e.printStackTrace();
+        }
+    }
 }
