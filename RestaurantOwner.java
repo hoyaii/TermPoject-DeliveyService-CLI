bafa9011b122f3094447c419dba4af6647b268b9
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-21 14:32:24 +0900

    feat: 주문 처리 구현

--- RestaurantOwner.java
+++ RestaurantOwner.java
@@ -51,4 +51,17 @@ public class RestaurantOwner {
             e.printStackTrace();
         }
     }
+
+    public void processOrder(int orderId, String status) {
+        String sql = "UPDATE `Order` SET order_status = ? WHERE order_id = ?";
+        try {
+            PreparedStatement preparedStatement = this.db.connection.prepareStatement(sql);
+            preparedStatement.setString(1, status);
+            preparedStatement.setInt(2, orderId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error executing SQL query.");
+            e.printStackTrace();
+        }
+    }
 }
