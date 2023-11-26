d02ef2427f0036d37dd31fc324a6c10e788ee7ab
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-26 23:44:45 +0900

    feat: 로그아웃 기능 구현

--- main.java
+++ main.java
@@ -52,135 +52,162 @@ class Main {
     public static void handleCustomer(Database db, int userId) {
         Customer customer = new Customer(db, userId);
 
-        System.out.println("고객님, 환영합니다! 아래 옵션 중 선택해 주세요:");
-        System.out.println("1. 음식점 검색과 주문");
-        System.out.println("2. 배달 상태 확인");
-        System.out.println("3. 리뷰 작성");
+        while(true) {
+            System.out.println("고객님, 환영합니다! 아래 옵션 중 선택해 주세요:");
+            System.out.println("1. 음식점 검색과 주문");
+            System.out.println("2. 배달 상태 확인");
+            System.out.println("3. 리뷰 작성");
+            System.out.println("4. 로그아웃");
 
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
-        scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
+            scanner.nextLine();
 
-        switch (option) {
-            case 1:
-                customer.searchRestaurantsService();
+            if (option == 4) { // 로그아웃 선택
                 break;
+            }
 
-            case 2:
-                customer.getDeliveryStatusService();
-                break;
+            switch (option) {
+                case 1:
+                    customer.searchRestaurantsService();
+                    break;
 
-            case 3:
-                customer.writeReviewService();
-                break;
+                case 2:
+                    customer.getDeliveryStatusService();
+                    break;
 
-            default:
-                System.out.println("잘못된 선택입니다.");
-                break;
+                case 3:
+                    customer.writeReviewService();
+                    break;
+
+                default:
+                    System.out.println("잘못된 선택입니다.");
+                    break;
+            }
         }
     }
 
     public static void handleRestaurantOwner(Database db, int userId) {
         RestaurantOwner restaurantOwner = new RestaurantOwner(db, userId);
 
-        System.out.println("사장님, 환영합니다! 아래 옵션 중 선택해 주세요:");
-        System.out.println("1. 음식점 정보 등록");
-        System.out.println("2. 음식점 정보 업데이트");
-        System.out.println("3. 메뉴 관리");
-        System.out.println("4. 조리 완료 처리하기");
-        System.out.println("5. 주문 이력 조회");
+        while (true) {
+            System.out.println("사장님, 환영합니다! 아래 옵션 중 선택해 주세요:");
+            System.out.println("1. 음식점 정보 등록");
+            System.out.println("2. 음식점 정보 업데이트");
+            System.out.println("3. 메뉴 관리");
+            System.out.println("4. 조리 완료 처리하기");
+            System.out.println("5. 주문 이력 조회");
+            System.out.println("6. 로그아웃");
 
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
+            scanner.nextLine();
 
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
-        scanner.nextLine();
-
-        switch (option) {
-            case 1:
-                restaurantOwner.registerRestaurantService();
+            if (option == 6) { // 로그아웃 선택
                 break;
+            }
 
-            case 2:
-                restaurantOwner.updateRestaurantInfoService();
-                break;
+            switch (option) {
+                case 1:
+                    restaurantOwner.registerRestaurantService();
+                    break;
 
-            case 3:
-                restaurantOwner.manageMenuService();
-                break;
+                case 2:
+                    restaurantOwner.updateRestaurantInfoService();
+                    break;
 
-            case 4:
-                restaurantOwner.finishCookingService();
-                break;
+                case 3:
+                    restaurantOwner.manageMenuService();
+                    break;
 
-            case 5:
-                restaurantOwner.getOrderHistoryService();
-                break;
+                case 4:
+                    restaurantOwner.finishCookingService();
+                    break;
 
-            default:
-                System.out.println("잘못된 선택입니다.");
-                break;
+                case 5:
+                    restaurantOwner.getOrderHistoryService();
+                    break;
+
+                default:
+                    System.out.println("잘못된 선택입니다.");
+                    break;
+            }
         }
     }
 
     public static void handleDeliveryPerson(Database db, int userId) {
         DeliveryPerson deliveryPerson = new DeliveryPerson(db, userId);
 
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
-        scanner.nextLine();
+        while (true) {
+            System.out.println("배달원님, 환영합니다! 아래 옵션 중 선택해 주세요:");
+            System.out.println("1. 배달 요청 확인 및 수락");
+            System.out.println("2. 배달 완료 처리하기");
+            System.out.println("3. 완료한 배달 이력 확인");
+            System.out.println("4. 로그아웃");
 
-        System.out.println("배달원님, 환영합니다! 아래 옵션 중 선택해 주세요:");
-        System.out.println("1. 배달 요청 확인 및 수락");
-        System.out.println("2. 배달 완료 처리하기");
-        System.out.println("3. 완료한 배달 이력 확인");
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
+            scanner.nextLine();
 
-        switch (option) {
-            case 1:
-                deliveryPerson.manageDeliveryRequestService();
+            if (option == 4) { // 로그아웃 선택
                 break;
+            }
 
-            case 2:
-                deliveryPerson.finishDeliveryService();
-                break;
+            switch (option) {
+                case 1:
+                    deliveryPerson.manageDeliveryRequestService();
+                    break;
 
-            case 3:
-                deliveryPerson.printDeliveryHistoryService();
-                break;
+                case 2:
+                    deliveryPerson.finishDeliveryService();
+                    break;
 
-            default:
-                System.out.println("잘못된 선택입니다.");
-                break;
+                case 3:
+                    deliveryPerson.printDeliveryHistoryService();
+                    break;
+
+                default:
+                    System.out.println("잘못된 선택입니다.");
+                    break;
+            }
         }
     }
 
     public static void handleServiceProvider(Database db) {
         ServiceProvider serviceProvider = new ServiceProvider(db);
 
-        System.out.println("관리자님, 환영합니다! 아래 옵션 중 선택해 주세요:");
-        System.out.println("1. 유저 계정 등록");
-        System.out.println("2. 유저 계정 수정");
-        System.out.println("3. 유저 계정 삭제");
+        while (true) {
+            System.out.println("관리자님, 환영합니다! 아래 옵션 중 선택해 주세요:");
+            System.out.println("1. 유저 계정 등록");
+            System.out.println("2. 유저 계정 수정");
+            System.out.println("3. 유저 계정 삭제");
+            System.out.println("4. 로그아웃");
 
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
-        scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
+            scanner.nextLine();
 
-        switch (option) {
-            case 1:
-                serviceProvider.registerUserService();
+            if (option == 4) { // 로그아웃 선택
                 break;
+            }
 
-            case 2:
-                serviceProvider.updateUserService();
-                break;
+            switch (option) {
+                case 1:
+                    serviceProvider.registerUserService();
+                    break;
 
-            case 3:
-                serviceProvider.deleteUserService();
-                break;
+                case 2:
+                    serviceProvider.updateUserService();
+                    break;
 
-            default:
-                System.out.println("잘못된 선택입니다.");
-                break;
+                case 3:
+                    serviceProvider.deleteUserService();
+                    break;
+
+                default:
+                    System.out.println("잘못된 선택입니다.");
+                    break;
+            }
         }
     }
 }
